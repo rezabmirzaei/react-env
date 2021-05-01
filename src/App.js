5e2e170a294c30317fffba18c2613a4523cc0254
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2021-05-01 11:50:37 +0200

    Testing env vars

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
     <div>
-      <small>This application is running in <b>{process.env.NODE_ENV}</b> mode.</small>
+      <p><small>This application is running in <b>{process.env.NODE_ENV}</b> mode.</small></p>
+      <p><small>Env var for testing has value: <b>{process.env.REACT_APP_NOT_SECRET_CODE}</b></small></p>
       {/* <form>
         <input type="hidden" defaultValue={process.env.REACT_APP_NOT_SECRET_CODE} />
       </form> */}
