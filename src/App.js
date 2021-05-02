5701f7d307439dd794a088bbf0eaf8d3592d0ba0
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2021-05-02 23:43:33 +0200

    Trigger CI/CD pipeline

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
       <p><small>proecss.env.NODE_ENV: <b>{process.env.NODE_ENV}</b></small></p>
       <p><small>process.env.REACT_APP_NOT_SECRET_CODE (environment var): <b>{process.env.REACT_APP_NOT_SECRET_CODE}</b></small></p>
       <p><small>process.env.ENV_VAR_KEY (from .env-file): <b>{process.env.ENV_VAR_KEY}</b></small></p>
+      
     </div>
   );
 }
