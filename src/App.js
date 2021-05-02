67146ab8210c3dba06bb09145f189751a23f13b3
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2021-05-02 22:54:26 +0200

    Delete azure pipeline def. Minor txt changes.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ export default function App() {
   return (
     <div className='m-4'>
       <p><small>proecss.env.NODE_ENV: <b>{process.env.NODE_ENV}</b></small></p>
-      <p><small>process.env.REACT_APP_NOT_SECRET_CODE (user env. var): <b>{process.env.REACT_APP_NOT_SECRET_CODE}</b></small></p>
-      <p><small>process.env.ENV_VAR_KEY (.env file var): <b>{process.env.ENV_VAR_KEY}</b></small></p>
+      <p><small>process.env.REACT_APP_NOT_SECRET_CODE (environment var): <b>{process.env.REACT_APP_NOT_SECRET_CODE}</b></small></p>
+      <p><small>process.env.ENV_VAR_KEY (from .env-file): <b>{process.env.ENV_VAR_KEY}</b></small></p>
     </div>
   );
 }
