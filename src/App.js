368571339c72294e5b7739b3fbf4a7890a827c9b
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2021-05-02 23:09:07 +0200

    Trigger CI/CD pipeline

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 export default function App() {
   return (
-    <div className='m-4'>
+    <div className='ml-5 mt-4'>
       <p><small>proecss.env.NODE_ENV: <b>{process.env.NODE_ENV}</b></small></p>
       <p><small>process.env.REACT_APP_NOT_SECRET_CODE (environment var): <b>{process.env.REACT_APP_NOT_SECRET_CODE}</b></small></p>
       <p><small>process.env.ENV_VAR_KEY (from .env-file): <b>{process.env.ENV_VAR_KEY}</b></small></p>
