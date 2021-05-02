8983d8d6ca7cccca144190c8245de293dbd64eb3
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2021-05-02 14:40:43 +0200

    Add dotenv dependency

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
+
 export default function App() {
   return (
     <div className='m-4'>
-      <p><small>This application is running in <b>{process.env.NODE_ENV}</b> mode.</small></p>
-      <p><small>Env var for testing has value: <b>{process.env.REACT_APP_NOT_SECRET_CODE}</b></small></p>
+      <p><small>proecss.env.NODE_ENV: <b>{process.env.NODE_ENV}</b></small></p>
+      <p><small>process.env.REACT_APP_NOT_SECRET_CODE (user env. var): <b>{process.env.REACT_APP_NOT_SECRET_CODE}</b></small></p>
+      <p><small>process.env.ENV_VAR_KEY (.env file var): <b>{process.env.ENV_VAR_KEY}</b></small></p>
     </div>
   );
 }
