21ea4b64500c2cd7cfa5f128b6a9eefb99823d46
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2021-05-03 10:16:04 +0200

    Setup for testing on firebase

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ require('dotenv').config()
 export default function App() {
   return (
     <div className='ml-5 mt-4'>
-      <p><small>proecss.env.NODE_ENV: <b>{process.env.NODE_ENV}</b></small></p>
-      <p><small>process.env.REACT_APP_NOT_SECRET_CODE (environment var): <b>{process.env.REACT_APP_NOT_SECRET_CODE}</b></small></p>
-      <p><small>process.env.REACT_APP_ENV_FILE_VAR_KEY (from .env-file): <b>{process.env.REACT_APP_ENV_FILE_VAR_KEY}</b></small></p>
+      <p>{process.env.REACT_APP_AUTH_DOMAIN}</p>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const app = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+});
+
+export default app;
