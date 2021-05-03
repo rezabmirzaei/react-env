e42f4bef59d117b06d2237674ea90ddf214de89c
Reza Mirzaei <reza.b.mirzaei@outlook.com>
2021-05-03 08:50:59 +0200

    Set correct var key

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ export default function App() {
     <div className='ml-5 mt-4'>
       <p><small>proecss.env.NODE_ENV: <b>{process.env.NODE_ENV}</b></small></p>
       <p><small>process.env.REACT_APP_NOT_SECRET_CODE (environment var): <b>{process.env.REACT_APP_NOT_SECRET_CODE}</b></small></p>
-      <p><small>process.env.ENV_VAR_KEY (from .env-file): <b>{process.env.ENV_VAR_KEY}</b></small></p>
-      
+      <p><small>process.env.REACT_APP_ENV_FILE_VAR_KEY (from .env-file): <b>{process.env.REACT_APP_ENV_FILE_VAR_KEY}</b></small></p>
     </div>
   );
 }
